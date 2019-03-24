4b266e1408be971d9399a63decf4916b584601e3
piotr Wolski <p.wolski1000@gmail.com>
2019-03-24 11:25:12 +0100

    fixes header width

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,22 +4,26 @@ import './../../css/header.scss'
 class index extends Component {
     render() {
         return (
-            <header className = "header">
-                <div className = "header__logo">
-                    buje
-                </div>
+            <div className="header__wrapper">
+                <header className = "header">
+                    <div className = "header__logo">
+                        <a href="/">
+                            buje
+                        </a>
+                    </div>
 
-                <nav className = "header__nav">
-                    <ul>
-                        <li><a href="#">home</a></li>
-                        <li><a href="#">service</a></li>
-                        <li><a href="#">portfolio</a></li>
-                        <li><a href="#">pricing</a></li>
-                        <li><a href="#">contact</a></li>
-                    </ul>
-                </nav>
+                    <nav className = "header__nav">
+                        <ul>
+                            <li><a href="/">home</a></li>
+                            <li><a href="/">service</a></li>
+                            <li><a href="/">portfolio</a></li>
+                            <li><a href="/">pricing</a></li>
+                            <li><a href="/">contact</a></li>
+                        </ul>
+                    </nav>
 
-            </header>
+                </header>
+            </div>
         );
     }
 }
