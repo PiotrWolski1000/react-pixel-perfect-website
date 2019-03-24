6bd12506766e809c54800b0f5a4d554549249a97
piotr Wolski <p.wolski1000@gmail.com>
2019-03-24 22:56:48 +0100

    adding footer section

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import SocialMedia from './../SocialMedia/'
+import './../../css/footer.scss'
 
 class index extends Component {
     render() {
         return (
-            <div>
-                footer
-            </div>
+            <footer className="footer">
+                <div className="footer__content__wrapper">
+                    
+                    <div className="footer__left__side">
+                        <p>buje</p>
+                        <hr></hr>
+                        <p><span>Buje.com</span> © 2014 Powered by <span>Wordpress</span></p>
+                    </div>
+
+                    <SocialMedia/>
+                </div>
+            </footer>
         );
     }
 }
