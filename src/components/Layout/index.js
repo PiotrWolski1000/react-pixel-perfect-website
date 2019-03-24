862b4c050d6d2e78f75c66a6d2f56e7793721600
piotr Wolski <p.wolski1000@gmail.com>
2019-03-24 11:27:36 +0100

    changing wrapper tag

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,18 +3,14 @@ import Header from '../Header/'
 import Footer from '../Footer/'
 
 class index extends Component {
-    constructor(props){
-        super(props)
-    }
-
     render() {
 
         return (
-            <div>
+            <>
                 <Header></Header>
                     {this.props.children}
                 <Footer></Footer>
-            </div>
+            </>
         )
     }
 }
