80a3f203485cd8b8ed33ce09f5e36265598ef1b3
piotr Wolski <p.wolski1000@gmail.com>
2019-04-04 15:48:22 +0200

    corrects form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -37,7 +37,7 @@ class index extends Component {
         }
         
         else if(name === 'email'){
-            if(value.length > 4 && 
+            if(value.length > 8 && 
                 value.includes('@') && 
                 value.includes('.')
             ){
@@ -188,7 +188,7 @@ class index extends Component {
                 <h1>{this.props.title}</h1>
                 <form 
                     name="contact-form"
-                    className='contact__form'
+                    className={this.state.subtopic?'contact__form form__extended':'contact__form'}
                     action="/"
                 >
                     <input 
